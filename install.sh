c8f74d2a9bacdcb50cb320e4b573be2c4cd5ef45
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-01-27 16:55:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ pacman -Sy git                                                    #installs git
 git clone https://github.com/ChrisTitusTech/ArchTitus             #download repository
 cd ArchTitus                                                      #opens downloaded folder
 ./archtitus.sh                                                    #runs installation script
-./apps.sh                                                                  #script to install more apps
-./plymouthinstall.sh                                                                  #script to install, and setup plymouth
+./apps.sh                                                         #script to install more apps
+./plymouthinstall.sh                                              #script to install, and setup plymouth
 ./plymouthsetup.sh																                                  #script to set desktop background
 																  
