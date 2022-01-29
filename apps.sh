4f3595099a3a27d9b12f45f25c8f26546d9c9d22
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-01-29 21:08:08 +0530

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,12 +1,11 @@
-#krita
-#ungoogled-chromium
-#neofetch
-#steam
-#firefox esr
-#something libreoffice
-#obs-studio
-#kdeconnect
-#telegram client
-#discord client
-#timeshift
-#kdenlive
+sudo pacman -Sy krita
+sudo pacman -Sy ungoogled-chromium
+sudo pacman -Sy neofetch
+sudo pacman -Sy steam
+sudo pacman -Sy firefox esr
+sudo pacman -Sy obs-studio
+sudo pacman -Sy kdeconnect
+sudo pacman -Sy telegram
+sudo pacman -Sy discord
+sudo pacman -Sy timeshift
+sudo pacman -Sy kdenlive
