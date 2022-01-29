1b75ed03b527fc077b3406dd80e421499993554c
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-01-29 20:22:05 +0530

    Update installerscript.sh

--- installerscript.sh
+++ installerscript.sh
@@ -1,4 +1,4 @@
-pacman -Sy git
+sudo pacman -Sy git
 git clone https://github.com/sounddrill31/Unnamed-ArchTitus-Based-Distro.git
 cd Unnamed-ArchTitus-Based-Distro
 sudo bash /Unnamed-ArchTitus-Based-Distro/install.sh
