05398a810fd4ebee204c11fb2c4a9fc046e9e8bb
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-02-02 19:49:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 echo "This is going to connect to internet, and use a lot of data, so avoid if you don't have unlimited network"
-pacman -Sy --yes git                                                   #installs git
+pacman -Sy git                                                   #installs git
 git clone https://github.com/ChrisTitusTech/ArchTitus             #download repository
 cd ArchTitus                                                      #opens downloaded folder
-./archtitus.sh                                                    #runs installation script
-sudo bash apps.sh --yes                                                         #script to install more apps
+sudo bash archtitus.sh                                                    #runs installation script
+#sudo bash apps.sh --yes                                                         #script to install more apps
 #./plymouthinstall.sh                                              #script to install, and setup plymouth
 #./plymouthsetup.sh			                          
 #sudo pacman -S feh                                       #install feh for bottom                                                                  
